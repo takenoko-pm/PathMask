2bfeb1969e0410a9fe7f6077c81ebdaa3e750759
takenoko-pm <skazu0611@icloud.com>
2026-03-09 12:34:15 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -3,31 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"regexp"
 	"runtime"
 
 	"github.com/atotto/clipboard"
 )
 
-func checkClipboardCmd() {
-	if runtime.GOOS != "linux" {
-		return
-	}
-	// xclipかxselのどちらかがあるか確認
-	if _, err := exec.LookPath("xclip"); err == nil {
-		return
-	}
-	if _, err := exec.LookPath("xsel"); err == nil {
-		return
-	}
-	fmt.Fprintln(os.Stderr, "Linuxでクリップボード操作するには xclip または xsel が必要です")
-	os.Exit(1)
-}
-
 func main() {
-	//Linuxのxclipかxsel確認
-	checkClipboardCmd()
 	// クリップボードを読み込む
 	data, err := clipboard.ReadAll()
 	if err != nil {
@@ -38,28 +20,24 @@ func main() {
 		return // クリップボードが空なら何もしない
 	}
 	text := string(data)
-	// ユーザー名部分を検出する正規表現
-	var re *regexp.Regexp
-	switch runtime.GOOS {
-	case "windows":
-		// Windowsの場合: C:\Users\tarou\ → C:\Users\<USER>\
-		re = regexp.MustCompile(`Users\\[^\\]+`)
-	case "darwin":
-		// Macの場合: /Users/tarou/ → Users/<USER>/
-		re = regexp.MustCompile(`/Users/[^/]+`)
-	case "linux":
-		// Linuxの場合: /home/tarou/ → home/<USER>/
-		re = regexp.MustCompile(`/home/[^/]+`)
-	}
 	// 置換
+	var re *regexp.Regexp // ユーザー名部分を検出する正規表現
 	var replaced string
 	switch runtime.GOOS {
 	case "windows":
-		replaced = re.ReplaceAllString(text, "Users\\<USER>")
+		// Windowsの場合: C:\Users\tarou\ → C:\Users\[USER]\
+		re = regexp.MustCompile(`Users\\[^\r\n\\>]+`)
+		replaced = re.ReplaceAllString(text, "Users\\[USER]")
 	case "darwin":
-		replaced = re.ReplaceAllString(text, "/Users/<USER>")
+		// Macの場合: /Users/tarou/ → Users/[USER]/
+		re = regexp.MustCompile(`/Users/[^\r\n/>]+`)
+		replaced = re.ReplaceAllString(text, "/Users/[USER]")
 	case "linux":
-		replaced = re.ReplaceAllString(text, "/home/<USER>")
+		// Linuxの場合: /home/tarou/ → home/[USER]/
+		re = regexp.MustCompile(`/home/[^\r\n/>]+`)
+		replaced = re.ReplaceAllString(text, "/home/[USER]")
+	default:
+		return
 	}
 	// 出力
 	fmt.Print(replaced)

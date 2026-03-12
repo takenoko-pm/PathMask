5a811fd9df34392116e7187bbb901bf689ecd102
takenoko-pm <skazu0611@icloud.com>
2026-03-12 17:13:56 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -2,47 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"regexp"
-	"runtime"
 
 	"github.com/atotto/clipboard"
 )
 
 func main() {
-	// クリップボードを読み込む
-	data, err := clipboard.ReadAll()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "読み込みエラー:", err)
-		return
+	text := ""
+	isClip := false
+	// 標準入力があるか確認
+	if file, _ := os.Stdin.Stat(); (file.Mode() & os.ModeCharDevice) == 0 {
+		// 標準入力から読み込む
+		data, err := io.ReadAll(os.Stdin)
+		if err == nil {
+			text = string(data)
+		}
 	}
-	if data == "" {
-		return // クリップボードが空なら何もしない
+	// 標準入力が空ならクリップボードを読む
+	if text == "" {
+		data, err := clipboard.ReadAll()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "クリップボード読み込みエラー:", err)
+			return
+		}
+		text = data
+		isClip = true
 	}
-	text := string(data)
+	if text == "" {
+		return // どちらも空なら終了
+	}
 	// 置換
-	var re *regexp.Regexp // ユーザー名部分を検出する正規表現
-	var replaced string
-	switch runtime.GOOS {
-	case "windows":
-		// Windowsの場合: C:\Users\tarou\ → C:\Users\[USER]\
-		re = regexp.MustCompile(`Users\\[^\r\n\\>]+`)
-		replaced = re.ReplaceAllString(text, "Users\\[USER]")
-	case "darwin":
-		// Macの場合: /Users/tarou/ → Users/[USER]/
-		re = regexp.MustCompile(`/Users/[^\r\n/>]+`)
-		replaced = re.ReplaceAllString(text, "/Users/[USER]")
-	case "linux":
-		// Linuxの場合: /home/tarou/ → home/[USER]/
-		re = regexp.MustCompile(`/home/[^\r\n/>]+`)
-		replaced = re.ReplaceAllString(text, "/home/[USER]")
-	default:
-		return
-	}
+	// ユーザー名部分を検出する正規表現
+	re := regexp.MustCompile(`(?i)([/\\])(Users|home)([/\\])[^/\r\n\\>]+`)
+	replaced := re.ReplaceAllString(text, "${1}${2}${3}[USER]")
 	// 出力
 	fmt.Print(replaced)
-	// クリップボードに戻す
-	if err := clipboard.WriteAll(replaced); err != nil {
-		fmt.Fprintln(os.Stderr, "クリップボード書き込みエラー:", err)
+	// クリップボードから読んでいたらクリップボードに戻す
+	if isClip {
+		if err := clipboard.WriteAll(replaced); err != nil {
+			fmt.Fprintln(os.Stderr, "クリップボード書き込みエラー:", err)
+		}
 	}
 }
